75573964cc289f7e96c1f6f8c6526ce628768a59
Andy Elkin <aelkin@gmail.com>
2021-04-09 10:26:45 -0400

    crash in parser (sn)

--- hopper_price.py
+++
@@ -1,5 +0,0 @@
-
-
-
-#https://deals.hopper.com/deals/hotels/list?checkinDate=2020-09-13&checkoutDate=2020-09-15&searchLocation=Hard%20Rock%20Hotel%20Daytona%20Beach%2C%20North%20Atlantic%20Avenue%2C%20Daytona%20Beach%2C%20FL%2C%20USA&source=SearchTerm
-
